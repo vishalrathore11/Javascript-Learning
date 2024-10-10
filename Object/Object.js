e6f71fa303c17c7f53ee76253f6a68f42940f918
vishalrathore11 <vishalwork1162@gmail.com>
2024-10-10 09:47:37 +0000

    enhanced obj literal

--- Object/Object.js
+++ Object/Object.js
@@ -1,18 +1,29 @@
 //Object literal
 
-const employee =  {
-    name : 'vishal',
-    age: 22,
-    'is Active': true,
-    getEmployeeInfo: function(city){
-       return `Employee name is ${this.name} and status is ${this.isActive} from ${city}` 
-    }
+// const employee =  {
+//     name : 'vishal',
+//     age: 22,
+//     'is Active': true,
+//     getEmployeeInfo: function(city){
+//        return `Employee name is ${this.name} and status is ${this.isActive} from ${city}` 
+//     }
+// }
+
+// employee.project = 'Coty';
+// employee.age = 'hhjlk';
+
+// console.log(employee.getEmployeeInfo('Bangalore'))
+// console.log('ages' in employee)
+
+//Enhanced object literal
+
+let name = 'vinoth';
+let salary = 500;
+
+const person ={
+    fname:name,salary
 }
 
-employee.project = 'Coty';
-employee.age = 'hhjlk';
-
-console.log(employee.getEmployeeInfo('Bangalore'))
-console.log(employee.age)
+console.log(person)
 
 
